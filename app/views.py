31fbe1b4e63931b84af88614cf91ac3734d847d3
gnchaithanya <chaithugajjala28@gmail.com>
2023-09-16 18:08:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def data_from_url(request,Name):
+    return HttpResponse(f'hii {Name} how are you!!')
